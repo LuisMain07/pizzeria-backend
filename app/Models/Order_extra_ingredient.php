2ef2bc8c0768e6197292d513aad637b6b31dbaae
michael esteban garcia diaz <este54191@gmail.com>
2025-05-28 12:23:21 -0500

    configuracion de los controladores

--- app/Models/Order_extra_ingredient.php
+++ app/Models/Order_extra_ingredient.php
@@ -10,6 +10,21 @@ class Order_extra_ingredient extends Model
     use HasFactory;
     protected $table = 'order_extra_ingredient';
     protected $primaryKey = 'id';
-    public $timestamps = false;
+    public $timestamps = true;
+
+    protected $fillable = [
+        'order_pizza_id',
+        'extra_ingredient_id',
+        'quantity',
+    ];
+
+    public function orders()
+    {
+        return $this->belongsTo(Order_pizza::class, 'order_pizza_id');
+    }
+    public function extraIngredient()
+    {
+        return $this->belongsTo(Extra_ingredient::class, 'extra_ingredient_id');
+    }
 }
 
