38ab2c6982a0c6ab524d71da99f320a80d2bb0e5
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-28 16:44:06 -0500

    se agrego la ruta del controlador api tamaño de pizza

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\PizzasController;
+use App\Http\Controllers\api\PizzasSizeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,4 +11,7 @@ Route::get('/user', function (Request $request) {
 
 // ruta de pizzas
 Route::apiResource('pizzas', PizzasController::class);
+
+// ruta de tamaños de pizzas
+Route::apiResource('pizzas_sizes', PizzasSizeController::class);
 
