a1e646a267d23efc4b1cea8944d6c5d5905a8d20
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-28 17:22:02 -0500

    se agrgeo la ruta de ingredientes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\PizzasController;
 use App\Http\Controllers\api\PizzasSizeController;
+use App\Http\Controllers\api\IngredientController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -14,4 +15,8 @@ Route::apiResource('pizzas', PizzasController::class);
 
 // ruta de tamaños de pizzas
 Route::apiResource('pizzas_sizes', PizzasSizeController::class);
+
+// ruta de ingredientes
+Route::apiResource('ingredients', IngredientController::class);
+
 
