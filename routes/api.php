23b623f1c57d2c03100eee2909dea9bb964a9370
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-29 12:03:12 -0500

    correccion

--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,15 @@ use App\Http\Controllers\api\IngredientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();    
+    return $request->user();
 });
 
-// ruta de pizzas
+// === RUTAS MÓDULO PIZZAS ===
 Route::apiResource('pizzas', PizzasController::class);
 
-// ruta de tamaños de pizzas
+// === RUTAS MÓDULO TAMAÑOS DE PIZZAS ===
 Route::apiResource('pizzas_sizes', PizzasSizeController::class);
 
-// ruta de ingredientes
+// === RUTAS MÓDULO INGREDIENTES ===
 Route::apiResource('ingredients', IngredientController::class);
-
-
